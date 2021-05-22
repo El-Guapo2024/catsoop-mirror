88f68ab5e850fc9474f1c74590acb87840ef2920
adam j hartz <hz@mit.edu>
2021-05-22 15:30:57 -0400

    revert logging mechanisms, remove postgres option

--- setup.py
+++ setup.py
@@ -212,7 +212,6 @@ def main():
                 "catsoop.test",
                 "catsoop.thirdparty",
                 "catsoop.scripts",
-                "catsoop.cslog",
             ],
             scripts=[],
             url="https://catsoop.mit.edu",
